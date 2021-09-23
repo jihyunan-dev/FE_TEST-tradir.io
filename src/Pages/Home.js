3a806f74881a7a8207ad3552c517562c9c4d9ab1
An Jihyun <hwiyu25@gmail.com>
2021-09-23 21:46:46 +0900

    📝 정보 안내 문구 추가

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
 
 const Home = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Container>
+      <Intro>Tradir 프론트엔드 과제 제출_안지현</Intro>
+      <GithubLink
+        href="https://github.com/jihyunan-dev/FE_TEST-tradir.io"
+        target="_blank"
+      >
+        Github Link 바로가기
+      </GithubLink>
+    </Container>
+  );
+};
 
-export default Home
+const Container = styled.div`
+  padding: 30px;
+`;
+
+const Intro = styled.div`
+  font-size: 24px;
+  font-weight: 600;
+  margin-bottom: 20px;
+`;
+
+const GithubLink = styled.a`
+  text-decoration: underline;
+`;
+
+export default Home;
